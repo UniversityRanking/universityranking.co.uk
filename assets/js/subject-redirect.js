c19492938a9a6eb8a6f2431ca73c429d5c7182f4
UniversityRanking <mail@af-g.uk>
2025-04-02 09:08:10 +0100

    Create subject-redirect.js

---
+++ assets/js/subject-redirect.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdown = document.getElementById("subject-select");
+  if (dropdown) {
+    dropdown.addEventListener("change", function () {
+      const subject = this.value;
+      if (subject) {
+        const encoded = encodeURIComponent(subject);
+        window.location.href = `/subject.html?subject=${encoded}`;
+      }
+    });
+  }
+});
